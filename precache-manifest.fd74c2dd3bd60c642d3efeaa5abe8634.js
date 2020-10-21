96f28b35561b188d0188f345ac603b8449ce75a0
tahirmayo <68633605+tahirmayo@users.noreply.github.com>
2020-10-21 15:38:28 +0500

    Updates

---
+++ precache-manifest.fd74c2dd3bd60c642d3efeaa5abe8634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af4163bd7d7dae5d3490db0f93a9a55",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "7783ada6ece659ff7b9b",
+    "url": "/youtube-clone/static/css/main.3d4dab7e.chunk.css"
+  },
+  {
+    "revision": "ab2b8681936dbf9d0dda",
+    "url": "/youtube-clone/static/js/2.191c6d63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.191c6d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7783ada6ece659ff7b9b",
+    "url": "/youtube-clone/static/js/main.8929ed6b.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
